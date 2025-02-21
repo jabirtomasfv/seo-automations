daf7ef01aa6493b2dd8ecce01b235cc1435fbd4d
jabirtomasfv <jabir.tomas@feverup.com>
2025-02-21 12:22:08 +0100

    Update keyword-clustering-app.py

--- keyword-clustering-app.py
+++ keyword-clustering-app.py
@@ -5,8 +5,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import DBSCAN
 import nltk
-nltk.download('punkt')
 from nltk.corpus import stopwords
+nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 import string
 
